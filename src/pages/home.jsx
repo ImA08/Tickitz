6369f2137115bb7a007b5576acf7f295c8dbe9af
Imanul Aufa <imanulaufa@gmail.com>
2025-03-13 10:12:28 +0700

    Add: home component

---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navbar } from "../components/Navbar.jsx";
+import { Footer } from "../components/Footer.jsx";
+
+
+export function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <Footer />
+    </>
+  );
+}
+
