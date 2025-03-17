2c313bd483b8d8fab7ae440b5860356d1f09036e
Imanul Aufa <imanulaufa@gmail.com>
2025-03-17 11:36:55 +0700

    add: link to direct to other path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export function Navbar() {
   };
 
   return (
-    <header>
+    <header className="sticky top-0 bg-white z-10">
       <nav className="flex flex-col items-center py-[22px] px-[10%] border-b border-solid border-[#e8e8e8] md:items-center md:justify-between md:flex-row md:px-[5%] lg:px-[10%]">
         {/* Logo dan Hamburger Menu */}
         <div className="md:w-auto w-full flex items-center justify-between">
@@ -27,13 +27,13 @@ export function Navbar() {
           } md:flex md:items-center md:gap-6 flex-col md:flex-row `}
         >
           <Link
-            
+            to="/"
             className="text-gray-800 font-medium hover:text-[#1d4ed8] transition duration-300 py-2 lg:py-0"
           >
             Home
           </Link>
           <Link
-            
+            to="movies"
             className="text-gray-800 font-medium hover:text-[#1d4ed8] transition duration-300 py-2 lg:py-0"
           >
             Movie
