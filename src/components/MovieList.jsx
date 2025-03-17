52d7b591090ca75fc19b8cba26acc8f571b6c6c5
Imanul Aufa <imanulaufa@gmail.com>
2025-03-17 10:16:10 +0700

    add: movie list component

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,150 @@
+import React from 'react'
+import recomend1 from "../assets/png/movie-h-1.png";
+
+
+export function MovieList() {
+  return (
+    <div className="grid grid-cols-[265px_265px_265px_265px] grid-rows-[490px] gap-4 h-[500px] overflow-hidden mb-2.5">
+            <div className="movie">
+              <div className="flex items-center justify-center">
+                <div
+                  style={{ backgroundImage: `url(${recomend1})` }}
+                  className="img flex relative items-center justify-center w-66 h-[405px] rounded-[5px] border border-(--primary-color) hover:scale-[1.1] group"
+                >
+                  <div className="detail absolute inset-0 invisible group-hover:visible group-hover:bg-black/70 flex flex-col gap-3 items-center justify-center">
+                    <a
+                      href=""
+                      className="w-[188px] h-12  text-white border border-white rounded-[5px] flex justify-center items-center hover:bg-(--primary-color) hover:text-white hover:scale-[1.1] "
+                    >
+                      Details
+                    </a>
+                    <a
+                      href=""
+                      className="w-[188px] h-12  text-white border border-white rounded-[5px] flex justify-center items-center hover:bg-(--primary-color) hover:text-white hover:scale-[1.1] "
+                    >
+                      Buy Ticket
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="desc mt-4">
+                <div className="movie-title text-[24px] font-bold">Black Widow</div>
+                <div className="genres flex gap-2 mt-3">
+                  <p className="flex items-center justify-center border border-(--secondary-color) rounded-[25px] bg-[rgba(160_163_189_0.1)] px-1.5 py-0.5 h-max w-max">
+                    Action
+                  </p>
+                  <p className="flex items-center justify-center border border-(--secondary-color) rounded-[25px] bg-[rgba(160_163_189_0.1)] px-1.5 py-0.5 h-max w-max">
+                    Adventure
+                  </p>
+                </div>
+              </div>
+            </div>
+    
+            <div className="movie">
+              <div className="flex items-center justify-center">
+                <div
+                  style={{ backgroundImage: `url(${recomend1})` }}
+                  className="img flex relative items-center justify-center w-66 h-[405px] rounded-[5px] border border-(--primary-color) hover:scale-[1.1] group"
+                >
+                  <div className="detail absolute inset-0 invisible group-hover:visible group-hover:bg-black/70 flex flex-col gap-3 items-center justify-center">
+                    <a
+                      href=""
+                      className="w-[188px] h-12  text-white border border-white rounded-[5px] flex justify-center items-center hover:bg-(--primary-color) hover:text-white hover:scale-[1.1] "
+                    >
+                      Details
+                    </a>
+                    <a
+                      href=""
+                      className="w-[188px] h-12  text-white border border-white rounded-[5px] flex justify-center items-center hover:bg-(--primary-color) hover:text-white hover:scale-[1.1] "
+                    >
+                      Buy Ticket
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="desc mt-4">
+                <div className="movie-title text-[24px] font-bold">Black Widow</div>
+                <div className="genres flex gap-2 mt-3">
+                  <p className="flex items-center justify-center border border-(--secondary-color) rounded-[25px] bg-[rgba(160_163_189_0.1)] px-1.5 py-0.5 h-max w-max">
+                    Action
+                  </p>
+                  <p className="flex items-center justify-center border border-(--secondary-color) rounded-[25px] bg-[rgba(160_163_189_0.1)] px-1.5 py-0.5 h-max w-max">
+                    Adventure
+                  </p>
+                </div>
+              </div>
+            </div>
+    
+            <div className="movie">
+              <div className="flex items-center justify-center">
+                <div
+                  style={{ backgroundImage: `url(${recomend1})` }}
+                  className="img flex relative items-center justify-center w-66 h-[405px] rounded-[5px] border border-(--primary-color) hover:scale-[1.1] group"
+                >
+                  <div className="detail absolute inset-0 invisible group-hover:visible group-hover:bg-black/70 flex flex-col gap-3 items-center justify-center">
+                    <a
+                      href=""
+                      className="w-[188px] h-12  text-white border border-white rounded-[5px] flex justify-center items-center hover:bg-(--primary-color) hover:text-white hover:scale-[1.1] "
+                    >
+                      Details
+                    </a>
+                    <a
+                      href=""
+                      className="w-[188px] h-12  text-white border border-white rounded-[5px] flex justify-center items-center hover:bg-(--primary-color) hover:text-white hover:scale-[1.1] "
+                    >
+                      Buy Ticket
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="desc mt-4">
+                <div className="movie-title text-[24px] font-bold">Black Widow</div>
+                <div className="genres flex gap-2 mt-3">
+                  <p className="flex items-center justify-center border border-(--secondary-color) rounded-[25px] bg-[rgba(160_163_189_0.1)] px-1.5 py-0.5 h-max w-max">
+                    Action
+                  </p>
+                  <p className="flex items-center justify-center border border-(--secondary-color) rounded-[25px] bg-[rgba(160_163_189_0.1)] px-1.5 py-0.5 h-max w-max">
+                    Adventure
+                  </p>
+                </div>
+              </div>
+            </div>
+    
+            <div className="movie">
+              <div className="flex items-center justify-center">
+                <div
+                  style={{ backgroundImage: `url(${recomend1})` }}
+                  className="img flex relative items-center justify-center w-66 h-[405px] rounded-[5px] border border-(--primary-color) hover:scale-[1.1] group"
+                >
+                  <div className="detail absolute inset-0 invisible group-hover:visible group-hover:bg-black/70 flex flex-col gap-3 items-center justify-center">
+                    <a
+                      href=""
+                      className="w-[188px] h-12  text-white border border-white rounded-[5px] flex justify-center items-center hover:bg-(--primary-color) hover:text-white hover:scale-[1.1] "
+                    >
+                      Details
+                    </a>
+                    <a
+                      href=""
+                      className="w-[188px] h-12  text-white border border-white rounded-[5px] flex justify-center items-center hover:bg-(--primary-color) hover:text-white hover:scale-[1.1] "
+                    >
+                      Buy Ticket
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="desc mt-4">
+                <div className="movie-title text-[24px] font-bold">Black Widow</div>
+                <div className="genres flex gap-2 mt-3">
+                  <p className="flex items-center justify-center border border-(--secondary-color) rounded-[25px] bg-[rgba(160_163_189_0.1)] px-1.5 py-0.5 h-max w-max">
+                    Action
+                  </p>
+                  <p className="flex items-center justify-center border border-(--secondary-color) rounded-[25px] bg-[rgba(160_163_189_0.1)] px-1.5 py-0.5 h-max w-max">
+                    Adventure
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+  )
+}
+
