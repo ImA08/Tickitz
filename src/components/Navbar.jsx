d4d935b62cd0a9b5636549c7620ba85934305cce
Imanul Aufa <imanulaufa@gmail.com>
2025-03-17 10:16:33 +0700

    add: Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,58 +1,70 @@
+import React, { useState } from "react";
+import HambugerMenu from "../assets/icon/hamburger-menu.svg";
 import logo from "../assets/svg/tickitz-1.svg";
-import { useEffect } from "react";
+import { Link } from "react-router";
 
 export function Navbar() {
-  useEffect(() => {
-    const toggleMenu = document.querySelector("#toggleMenu");
-    const sidebar = document.querySelector("#sidebar")
-    if (toggleMenu) {
-      toggleMenu.onclick = () => {
-        toggleMenu.classList.toggle("hamburger-toggle");
-        sidebar.classList.toggle("showNav");
-        
-      };
-    }
-  }, []);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <header className="sticky flex flex-col min-h-[104px] border-b border-black/10 bg-white">
-      <nav className="flex px-10 min-h-[104px] md:w-full md:px-24 justify-between items-center">
-        <a href="./index.html">
-          <img src={logo} alt="" />
-        </a>
-        <div className="hidden justify text-[#0f172a] md:flex md:justify-around md:gap-15">
-          <a href="index.html">Home</a>
-          <a href="./src/pages/movie-list.html">Movie</a>
-          <a href="">Buy Ticket</a>
+    <header>
+      <nav className="flex flex-col items-center py-[22px] px-[10%] border-b border-solid border-[#e8e8e8] md:items-center md:justify-between md:flex-row md:px-[5%] lg:px-[10%]">
+        {/* Logo dan Hamburger Menu */}
+        <div className="md:w-auto w-full flex items-center justify-between">
+          <img src={logo} alt="Tickitz Logo" />
+          <button className="lg:hidden focus:outline-none" onClick={toggleMenu}>
+            <img src={HambugerMenu} alt="Menu" className="md:hidden" />
+          </button>
         </div>
-        <div className="hidden md:flex md:gap-3 md:items-center md:justify-center">
-          <a
-            className="md:w-23 md:h-12 md:rounded-md gap-3 md:p-3 border border-(--primary-color) text-(--primary-color) md:flex items-center justify-center hover:bg-(--primary-color) hover:text-white"
-            href="./src/pages/signIn.html"
+        {/* Navbar Links */}
+        <div
+          className={`${
+            isMenuOpen ? "flex" : "hidden"
+          } md:flex md:items-center md:gap-6 flex-col md:flex-row `}
+        >
+          <Link
+            
+            className="text-gray-800 font-medium hover:text-[#1d4ed8] transition duration-300 py-2 lg:py-0"
           >
-            <div>SignIn</div>
-          </a>
-          <a
-            className="md:w-23 md:h-12 md:rounded-md gap-3 md:p-3 border border-(--primary-color) text-(--primary-color) md:flex items-center justify-center hover:bg-(--primary-color) hover:text-white"
-            href="./src/pages/register.html"
+            Home
+          </Link>
+          <Link
+            
+            className="text-gray-800 font-medium hover:text-[#1d4ed8] transition duration-300 py-2 lg:py-0"
           >
-            <div>Sign Up</div>
-          </a>
+            Movie
+          </Link>
+          <Link
+            
+            className="text-gray-800 font-medium hover:text-[#1d4ed8] transition duration-300 py-2 lg:py-0"
+          >
+            Buy Ticket
+          </Link>
         </div>
-        <div id="toggleMenu" className="hidden  place-content-center w-20 h-20">
-          <div className="md:hidden w-15 h-1.5 bg-black rounded-full transition-all duration-150 before:content-[''] before:absolute before:w-15 before:h-1.5 before:bg-black before:rounded-full before:transition-all before:duration-150 before:-translate-y-4 after:content-[''] after:absolute after:w-15 after:h-1.5 after:bg-black after:rounded-full after:transition-all after:duration-150 after:translate-y-4"></div>
+        {/* Navbar Buttons */}
+        <div
+          className={`${
+            isMenuOpen ? "flex" : "hidden"
+          } md:flex md:items-center md:gap-4 flex-col md:flex-row gap-4 w-full md:w-auto`}
+        >
+          <Link
+            to="auth"
+            className="bg-[#ffffff] text-[#1d4ed8] border border-solid border-[#1d4ed8] rounded-[6px] flex items-center justify-center w-full transition-all duration-200 ease-linear md:w-[86px] h-[48px] lg:w-auto lg:px-4 hover:bg-[#1d4ed8] hover:text-white"
+          >
+            Sign In
+          </Link>
+          <Link
+            to="auth/register"
+            className="bg-[#1d4ed8] text-[#ffffff] border border-solid border-[#1d4ed8] rounded-[6px] flex items-center justify-center w-full transition-all duration-200 ease-linear md:w-[86px] h-[48px] lg:w-auto lg:px-4 hover:bg-white hover:text-[#1d4ed8]"
+          >
+            Sign Up
+          </Link>
         </div>
-
-        
-
       </nav>
-            <div id="sideBar" className="hidden">
-                <ul className="flex flex-col gap-7 items-center py-2.5">
-                    <li><a className="h-9 text-2xl" href="#">Home</a></li>
-                    <li><a className="h-9 text-2xl" href="#">Movie</a></li>
-                    <li><a className="h-9 text-2xl" href="#">Buy Ticket</a></li>
-                </ul>
-             </div>
     </header>
   );
-}
+};
