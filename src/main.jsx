cd06ccde8923983bda797c4dbc477ef84034b062
Imanul Aufa <imanulaufa@gmail.com>
2025-03-24 15:43:28 +0700

    add: add some logic for separating route

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./main.css";
 import { Router } from "./router";
 import { BrowserRouter } from "react-router";
+
+import { UserProvider } from "./contexts/userContext";
 // import "../src/style/index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <StrictMode>    
+    <UserProvider>
     <BrowserRouter>
       <Router />
     </BrowserRouter>
+    </UserProvider>
   </StrictMode>
 );
