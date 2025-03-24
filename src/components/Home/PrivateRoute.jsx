76474ab338d115fdb70e4473b11fb337560068a7
Imanul Aufa <imanulaufa@gmail.com>
2025-03-24 15:20:23 +0700

    add: private route for routing system

---
+++ src/components/Home/PrivateRoute.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router";
+
+import { userContext } from "../../contexts/userContext";
+
+export function PrivateRoute({ children, redirectTo }) {
+  const { user } = useContext(userContext);
+  const location = useLocation();
+
+  // case 1: belum login
+  if (!user.email || !user.password) {
+    return <Navigate to={redirectTo} replace state={{ from: location.pathname }} />;
+  }
+
+
+  return children;
+}
+
+// export default PrivateRoute;
