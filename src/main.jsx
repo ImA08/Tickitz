8d9837644742c643a9cb4a5ad75f7413f0da2048
Imanul Aufa <imanulaufa@gmail.com>
2025-04-03 13:17:28 +0700

    add: provider for wrapping components for redux global state management

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./main.css";
 import { Router } from "./router";
 import { BrowserRouter } from "react-router";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 import { UserProvider } from "./contexts/userContext";
 // import "../src/style/index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>    
+  <StrictMode>
+    <Provider store={store}>
     <UserProvider>
     <BrowserRouter>
       <Router />
     </BrowserRouter>
     </UserProvider>
+    </Provider>
   </StrictMode>
 );
