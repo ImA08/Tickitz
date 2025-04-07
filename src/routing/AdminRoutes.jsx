bdc00de8ae43d03fd58c8665dde947fcd9b5bc3e
Imanul Aufa <imanulaufa@gmail.com>
2025-04-08 03:48:22 +0700

    fix some code

--- src/routing/AdminRoutes.jsx
+++ src/routing/AdminRoutes.jsx
@@ -4,11 +4,13 @@ import { PrivateRoute } from "../components/Home/PrivateRoute";
 import { AdminPage } from "../pages/adminPage";
 import { AdminNavbar } from "../components/admin/AdminNavbar";
 import { AdminMovie } from "../components/admin/AdminMovie";
+import { AdminDashboard } from "../components/admin/AdminDashboard";
+import { AddNewMovie } from "../components/admin/AddNewMovie";
 
 function AdminLayout() {
   return (
     <>
-    <AdminNavbar />
+      <AdminNavbar />
       <Outlet />
     </>
   );
@@ -18,9 +20,11 @@ export function AdminRoutes() {
   return (
     <>
       <Route path="admin" element={<AdminLayout />}>
+        <Route index element={<AdminDashboard />} />
         <Route path="movie" element={<AdminMovie />} />
+        <Route path="addmovie" element={<AddNewMovie />} />
+
       </Route>
     </>
   );
 }
-
